fbac571baef18ce14335069d224b99e925825be5
T-Raillard <raillard.tristan@gmail.com>
2021-04-02 18:29:59 +0200

    first commit, missing solve and back

---
+++ sudoku.py
@@ -0,0 +1,139 @@
+import pygame
+from sudoku_classes import Square, Button
+from sudoku_consts import *
+
+# initialize the window
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("    Sudoku Solver")
+icon = pygame.image.load(r'C:\Users\Tristan Raillard\Documents\Python\pygame\assets\ksudoku_103845 (1).ico')
+pygame.display.set_icon(icon)
+
+# create all 81 squares
+border_offset_x = 0
+border_offset_y = 0
+for i in range(9):
+    if i % 3 == 0 and i != 0:
+        border_offset_x += 1
+
+    for j in range(9):
+        if j % 3 == 0 and j != 0:
+            border_offset_y += 1
+
+        x = GRID_x + EXT_BORDER + i * (SQUARE_WIDTH + INN_BORDER) + border_offset_x
+        y = GRID_y + EXT_BORDER + j * (SQUARE_WIDTH + INN_BORDER) + border_offset_y
+        Square(x, y, i, j)
+
+    border_offset_y = 0     
+
+def main():
+    """Main fonction, loops through every frame"""
+    solve_button = Button("BACK", back)
+    clear_button = Button("CLEAR", clear)
+    check_button = Button("CHECK", check)
+    solve_button = Button("SOLVE", solve)
+
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                handle_click(*event.pos)
+            
+            if event.type == pygame.KEYDOWN:
+                handle_key(event.key)
+    
+        draw_window()
+    
+def draw_window():
+    """Called with each frame to draw the window"""
+    # background
+    WIN.fill(BG_COLOR)
+
+    # external border
+    pygame.draw.rect(WIN, EXT_BORDER_COLOR, pygame.Rect(GRID_x, GRID_y, GRID_WIDTH, GRID_WIDTH))
+
+    # inner border
+    for i in range(3):
+        for j in range(3):
+            x = GRID_x + EXT_BORDER + i * (3 * SQUARE_WIDTH + 4 * INN_BORDER)
+            y = GRID_y + EXT_BORDER + j * (3 * SQUARE_WIDTH + 4 * INN_BORDER)
+            width = 3 * SQUARE_WIDTH + 2 * INN_BORDER
+            pygame.draw.rect(WIN, INN_BORDER_COLOR, pygame.Rect(x, y, width, width))
+
+    # squares
+    for square in Square.all_unordered:
+        square.draw(WIN)
+
+    # buttons
+    for button in Button.all:
+        button.draw(WIN)
+
+    pygame.display.update()
+
+def handle_click(x, y):
+    """handles the event MOUSEBUTTONDOWN"""
+    # unselect squares, then looks if clicked
+    for square in Square.all_unordered:
+        if square.check_clicked(x, y):
+            square.selected = not square.selected
+            square.color = SEL_SQUARE_COLOR if square.selected else SQUARE_COLOR
+        else:
+            square.selected = False
+            square.color = SQUARE_COLOR    
+
+    # looks if a button is clicked
+    for button in Button.all:
+        if button.check_clicked(x, y):
+            button.click()
+
+def handle_key(key):
+    """handles the event KEYDOWN"""
+    # selects only numbers or backspace, or returns
+    key = pygame.key.name(key).strip('[').strip(']')
+    try:
+        if key != "backspace":
+            key = int(key)
+            assert key != 0
+    except (ValueError, AssertionError):
+        return
+    # looks for a selected square and writes key in it
+    for square in Square.all_unordered:
+        if square.selected:
+            if key == "backspace":
+                square.clear()
+            else:
+                square.write(key)
+            return
+
+def back():
+    """Callback fonction for button Back"""
+    print("back called !")
+
+def clear():
+    """Callback fonction for button Clear"""
+    for square in Square.all_unordered:
+        square.clear()
+
+def check():
+    """Callback fonction for button Check"""
+    all_legal = True
+    for square in Square.all_unordered:
+        if not square.legal:
+            square.number_color = RED
+            all_legal = False
+    
+    return all_legal
+
+def solve():
+    """Callback fonction for button Solve"""
+    pass
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ sudoku_classes.py
@@ -0,0 +1,127 @@
+import pygame
+from sudoku_consts import *
+
+pygame.font.init()
+FONT = pygame.font.Font(r'C:\Users\Tristan Raillard\Documents\Python\pygame\assets\Roboto-Regular.ttf', 30)
+FONT20 = pygame.font.Font(r'C:\Users\Tristan Raillard\Documents\Python\pygame\assets\Roboto-Regular.ttf', 20)
+
+class Button:
+    """Buttons on the right of the screen; all is an array of all instances"""
+
+    all = []
+
+    def __init__(self, msg, fct):
+        """Message gets printed on the button, fct is the the callback fonction when the the button is clicked"""
+        self.msg = msg
+        self.coord_x = 0
+        self.coord_y = 0
+        self.click = fct
+        Button.all.append(self)
+    
+    def draw(self, win):
+        """Call while drawing the window on the instance to add it to the caption"""
+        self.coord_x = WIDTH / 2 - OFFSET * 2.5
+        self.coord_y = (HEIGHT - BUTTON_HEIGHT) / 2 + (Button.all.index(self) - (len(Button.all) - 1) / 2) * BUTTON_HEIGHT * 1.7
+        pygame.draw.rect(win, BUTTON_COLOR, pygame.Rect(self.coord_x, self.coord_y, BUTTON_WIDTH, BUTTON_HEIGHT), border_radius=15)
+
+        msg = FONT20.render(self.msg, False, BUTTON_FONT_COLOR)
+        win.blit(msg, (self.coord_x + (BUTTON_WIDTH - msg.get_rect().width) / 2, self.coord_y + (BUTTON_HEIGHT - msg.get_rect().height) / 2))
+    
+    def check_clicked(self, x, y):
+        """Check from coordinates if they correspond to the instance's location"""
+        return self.coord_x < x < self.coord_x + SQUARE_WIDTH and self.coord_y < y < self.coord_y + SQUARE_WIDTH
+
+
+class Square:
+    """A square designed to fit in a nine by nine sudoku grid of squares"""
+
+    all = [[]]
+    all_unordered = []
+
+    def __init__(self, x, y, row, column):
+        """Must pass coordinates and their index(row, column) in oreder to draw properly"""
+        self.coord_x = x
+        self.coord_y = y
+        self.row = row
+        self.column = column
+        self.nine_square = row // 3 + (column // 3 * 3)
+        self.value = ""
+        self.legal = True
+        self.selected = False
+        self.impossible_values = set()
+        self.color = SQUARE_COLOR
+        self.number_color = SQUARE_FONT_COLOR
+
+        # append the instance to all and all_unordered when created
+        if len(Square.all[-1]) % 9 == 0 and len(Square.all[-1]) != 0:
+            Square.all.append([self])
+        else:
+            Square.all[-1].append(self)
+        Square.all_unordered.append(self)
+
+    def __repr__(self):
+        """Prettify for debugging"""
+        return f"{self.row}, {self.column}"
+
+    def draw(self, win):
+        """Call while drawing the window on the instance to add it to the caption"""
+        pygame.draw.rect(win, self.color, pygame.Rect(self.coord_x, self.coord_y, SQUARE_WIDTH, SQUARE_WIDTH))
+        value = FONT.render(str(self.value), False, self.number_color)
+        win.blit(value, (self.coord_x + (SQUARE_WIDTH - value.get_rect().width) / 2, self.coord_y + (SQUARE_WIDTH - value.get_rect().height) / 2))
+
+    def check_clicked(self, x, y):
+        """Check from coordinates if they correspond to the instance's location"""
+        return self.coord_x < x < self.coord_x + SQUARE_WIDTH and self.coord_y < y < self.coord_y + SQUARE_WIDTH
+    
+    def seen(self):
+        """Return an array of instances which are "seen" by this instance"""
+        rv = []
+
+        # look in the row
+        for square in Square.all[self.row]:
+            if square != self and square.value != "":
+                rv.append(square)
+        
+        # look in the column
+        for row in Square.all:
+            square = row[self.column]
+            if square != self and square.value != "":
+                rv.append(square)
+        
+        # look in the 3x3 square
+        for square in Square.all_unordered:
+            if square != self and square.nine_square == self.nine_square and square.value != "":
+                rv.append(square)
+
+        return rv
+
+    def write(self, value):
+        """Change the value of a square and check if it is a legal move"""
+        self.value = value
+        
+        for square in self.seen():
+            if self.value == square.value:
+                self.legal = False
+                square.legal = False
+    
+    def clear(self):
+        """Errase a value and set accordingly all seen square's "legality" """
+        for square in self.seen():
+            if self.value == square.value:
+                now_legal = True
+
+                for seen_square in square.seen():
+                    if seen_square.value == square.value and seen_square != self:
+                        now_legal = False
+
+                square.legal = now_legal
+                square.number_color = SQUARE_FONT_COLOR if now_legal else square.number_color
+                print('*')
+                        
+        self.value = ""
+        self.legal = True
+        self.number_color = SQUARE_FONT_COLOR
+    
+    def solve(self, unsolved, WIN):
+        pass
+        
